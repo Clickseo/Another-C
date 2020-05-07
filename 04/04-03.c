0b6c3dfe7fea27285dd71994a6dae4c30fae961b
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-08 01:38:00 +0900

    Update 04-03.c

--- 04/04-03.c
+++ 04/04-03.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-// 함수 정의 : OUTPUT() 함수 -- 메시지 출력 기능 수행
+// 함수 정의 : OUTPUT 함수
+// 메시지 출력 -- "Hello World!!!"
 void OUTPUT(void)
 {
 	printf("Hello World!!! \n");
@@ -8,7 +9,7 @@ void OUTPUT(void)
 
 int main(void)
 {
-	// 함수 호출
+	// 함수 호출 : OUTPUT 함수
 	OUTPUT();
 
 	return 0;
