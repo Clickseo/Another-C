c2b54d42a525ac6743d8cf977805d66e11e38d26
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-08 01:31:21 +0900

    Update 04-07.c

--- 04/04-07.c
+++ 04/04-07.c
@@ -4,7 +4,7 @@ int main(void)
 	int	a, b, c, d, max;
 
 	printf("4개의 정수 입력 : ");
-	scanf_("%d %d %d %d", &a, &b, &c, &d);		// scanf("%d %d %d %d", &a, &b, &c, &d);
+	scanf_s("%d %d %d %d", &a, &b, &c, &d);		// scanf("%d %d %d %d", &a, &b, &c, &d);
 
 	// 4개의 정수 값 중에서 최대값을 구하는 부분
 	if(a > b)	max = a;
