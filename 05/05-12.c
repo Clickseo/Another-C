6bd24544af3d4b8f706a38c48656e34b6aa615ad
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-09 02:04:10 +0900

    Update 05-12.c

--- 05/05-12.c
+++ 05/05-12.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
+
 #define MAX_SIZE 3
 
-// ��ü �迭 ���Ҹ� ����ϱ� ���� OUTPUT( ) �Լ� ���� ����
+// 함수 원형 : 전체 배열 원소를 출력하기 위한 OUTPUT 함수
 void	OUTPUT(int *, int);
 
 int main(void)
 {
-	int	arr[3] = {10, 20, 30};
+	int	arr[3] = { 10, 20, 30 };
 
-	// �迭 ��ü ���� ����� ���� OUTPUT( ) �Լ� ȣ��
+	// 함수 호출 : 배열 전체 원소 출력을 위한 OUTPUT 함수
 	OUTPUT(arr, MAX_SIZE);
 
 	return 0;
 }
 
-// ��ü �迭 ���Ҹ� ����ϱ� ���� OUTPUT( ) �Լ� ����
+// 함수 정의 : 전체 배열 원소를 출력하기 위한 OUTPUT 함수
 void	OUTPUT(int *p, int n)
 {
-	int	i;
-
-	for(i=0; i<n; i++)
+	for(int i=0; i<n; i++)
 		printf("arr[%d]: %d \n", i, *(p+i) );
 
 	return;
