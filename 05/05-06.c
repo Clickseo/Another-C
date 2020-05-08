d11ee1cd253f852c19ea8ab491f29b4b33561b63
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-09 01:57:43 +0900

    Update 05-06.c

--- 05/05-06.c
+++ 05/05-06.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 #define MAX_SIZE 3
+
 int main(void)
 {
-	int	i;
 	int	arr[MAX_SIZE];
 
-	for(i=0; i<MAX_SIZE; i++)
-		scanf("%d", arr + i );      // scanf("%d", &arr[i] );
+	for(int i=0; i<MAX_SIZE; i++)
+		scanf_s("%d", arr + i );      // scanf("%d", &arr[i] );
 
 	for(i=0; i<MAX_SIZE; i++)
 		// printf("arr[%d]: %d \n", i, arr[i] );
