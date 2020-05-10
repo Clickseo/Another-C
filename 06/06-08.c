9841ceaa2d43539c354f1c0f827a6197dc1fa634
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-10 13:52:00 +0900

    Update 06-08.c

--- 06/06-08.c
+++ 06/06-08.c
@@ -2,16 +2,16 @@
 #define MAX_SIZE 1024
 int main(void)
 {
-	char	name[] = "º≠µŒø¡";
-	int	kor = 70, eng = 80, math =90, tot;
+	char	name[] = "ÏÑúÎëêÏò•";
+	int	kor = 70, eng = 80, math = 90, tot;
 	double	ave;
-	char	str[MAX_SIZE];
+	
+	char	str[MAX_SIZE];		
 
 	tot = kor + eng + math;
 	ave = (kor + eng + math) / 3.0;
 
-	sprintf(str, "%10s %3d %3d %3d %5d %8.2f", 
-				name, kor, eng, math, tot, ave);
+	sprintf_s(str, sizeof(str), "%10s %3d %3d %3d %5d %8.2f", name, kor, eng, math, tot, ave);
 	printf("str : %s \n", str);
 
 	return 0;
