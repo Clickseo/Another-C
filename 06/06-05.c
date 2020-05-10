4b5e8dfcc25019bbec62aae025b9bdcd91cbf055
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-10 13:43:53 +0900

    Update 06-05.c

--- 06/06-05.c
+++ 06/06-05.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main(void)
 {
-	char	str[10];
+	char	str[12];
 
-	gets(str);						// test
+	// gets(str);
+	gets_s(str, sizeof(str));		// test
 	printf("str : %s \n", str);
 
-	fgets(str, sizeof(str)-1, stdin);		// test
+	fgets(str, sizeof(str) - 1, stdin);	// test
 	printf("str : %s \n", str);
 
 	return 0;
