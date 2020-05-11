80425c3e7091e2adf596cacd2546e91539305909
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-12 02:04:14 +0900

    Update 01-11.c

--- 01/01-11.c
+++ 01/01-11.c
@@ -3,8 +3,8 @@ int main(void)
 {
 	unsigned int	i = 4294967295;
 
-	printf("i : %d \n", i);	// -1
-	printf("i : %u \n", i);	// 4294967295
+	printf("i : %d \n", i);		// -1
+	printf("i : %u \n", i);		// 4294967295
 
 	return 0;
 }
