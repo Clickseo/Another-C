bcb0c5b9cdc6935f2f23337e67398c657bda4b47
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-14 13:09:23 +0900

    Update 01-08.c

--- 01/01-08.c
+++ 01/01-08.c
@@ -1,10 +1,11 @@
+// 다양한 정수형
 #include <stdio.h>
 int main(void)
 {
-	printf("    short int : %d바이트\n", sizeof(short int));
-	printf("          int : %d바이트\n", sizeof(int));
-	printf("     long int : %d바이트\n", sizeof(long int));
-	printf("long long int : %d바이트\n", sizeof(long long int));
+	printf("    short int : %d바이트\n", sizeof(short int));		// short
+	printf("          int : %d바이트\n", sizeof(int));			// int
+	printf("     long int : %d바이트\n", sizeof(long int));		// long
+	printf("long long int : %d바이트\n", sizeof(long long int));	// long long
 	
 	return 0;
 }
