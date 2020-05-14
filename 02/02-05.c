457bcd92b767a44c3cb7c4838a185713e775af49
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-14 12:22:02 +0900

    Update 02-05.c

--- 02/02-05.c
+++ 02/02-05.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-	char	ch;
-	int	i;
-
-	for(i = 1, ch = 'a' - 32; ch <= 'z' - 32; ch++, i++)
+	char	ch = 'a' - 32;					// char	ch = 'A';
+	for(int i = 1; ch <= ('z' - 32); ch++, i++)		// ('z' - 32) == 'Z'
 	{
 		if(i%5)	printf("%3c : %3d \t", ch, ch);
 		else	printf("%3c : %3d \n", ch, ch);
