2809a786337e492981b645d90b4c9773e35e68ad
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-14 12:35:33 +0900

    Update 02-12.c

--- 02/02-12.c
+++ 02/02-12.c
@@ -4,9 +4,9 @@ int main(void)
 	int	a = 0x0012FF7C;			// int	a = 1245052;
 	int	b = 0x0000FFFF;			// int	b = 65535;
 
-	printf("a & b : %p, %d\n", a&b, a&b);  // 0000FF7C, 65404
-	printf("a | b : %p, %d\n", a|b, a|b);  // 0012FFFF, 1245183
-	printf("a ^ b : %p, %d\n", a^b, a^b);  // 00120083, 1179779
+	printf("a & b : %08X, %d\n", a&b, a&b);  // 0000FF7C, 65404
+	printf("a | b : %08X, %d\n", a|b, a|b);  // 0012FFFF, 1245183
+	printf("a ^ b : %08X, %d\n", a^b, a^b);  // 00120083, 1179779
 
 	return 0;
 }
