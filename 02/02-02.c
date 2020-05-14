04da99e24dcb8235808af19a8dfb25dc861fc70c
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-14 12:17:06 +0900

    Update 02-02.c

--- 02/02-02.c
+++ 02/02-02.c
@@ -7,8 +7,8 @@ int main(void)
 	a = 10.5;
 	b = 10.5;
 
-	res1 = a + b;
-	res2 = (int)a + (int)b;
+	res1 = a + b;			// 데이터 손실 발생
+	res2 = (int)a + (int)b;		// (수동) 형 변환
 
 	printf("res1 : %d \n", res1);	// 21
 	printf("res2 : %d \n", res2);	// 20
