cb644d2cb9703c76c2020137ab9a8dbde9bdac74
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 07:00:25 +0900

    Update 04-13.c

--- 04/04-13.c
+++ 04/04-13.c
@@ -1,8 +1,9 @@
-int  Factorial(int  n)
+// 함수 원형 선언 : Factorial 함수 -- 비재귀적 용법(반복적인 용법)
+int  Factorial(int  num)
 {
-	int	i,  res = 1;
+	int	res = 1;
 
-	for (i = 1; i <= n; i++)
+	for (int i = 1; i <= num; i++)
 		res  =  res  *  i;
 
 	return  res;
