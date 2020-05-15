32f555ab5430c5e5e7c2bcdf3408a68f1461c183
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 06:27:57 +0900

    Update 03-19.c

--- 03/03-19.c
+++ 03/03-19.c
@@ -9,16 +9,16 @@ int main(void)
 	void	*p;
 
 	p = &a;
-	printf("a : %c \n", *(char *)p );
+	printf("a : %c \n", *(char *)p );	// a
 
 	p = &i;
-	printf("a : %d \n", *(int *)p );
+	printf("a : %d \n", *(int *)p );	// b
 
 	p = &f;
-	printf("a : %f \n", *(float *)p );
+	printf("a : %f \n", *(float *)p );	// c
 
 	p = &d;
-	printf("a : %f \n", *(double *)p );
+	printf("a : %f \n", *(double *)p );	// d
 
 	return 0;
 }
