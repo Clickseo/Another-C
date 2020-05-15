a66480799f254a2a36178299ca831a54ee172625
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 00:53:03 +0900

    Update 02-09.c

--- 02/02-09.c
+++ 02/02-09.c
@@ -7,7 +7,7 @@ int main(void)
 	ch = i;					// 데이터 손실 발생(4bytes --> 1byte)
 
 	printf("ch : %d \n", ch);		// -128
-	printf(" i : %d \n", i);		// 128
+	printf(" i : %d \n", i);		//  128
 
 	return 0;
 }
