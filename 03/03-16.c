d4f08f2ff98e7d82ea3477834f829f7edc77e2dc
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 06:22:22 +0900

    Update 03-16.c

--- 03/03-16.c
+++ 03/03-16.c
@@ -1,17 +1,20 @@
+// 포인터 연산 : 증감연산자(++, --)
 #include <stdio.h>
 int main(void)
 {
 	int	a = 10;
 	int	*p = &a;
 
-	printf("a : %d \n", *p );		// 10
-	printf("a : %d \n", (*p)++ );		// 10
+	printf("a : %d \n", *p );		// 10	<-- a	
+	printf("a : %d \n", (*p)++ );		// 10	<-- (*p)++ == a++
 
-	printf("a : %d \n", *p );		// 11
-	printf("a : %d \n", *p++ );		// 11
+	printf("a : %d \n", *p );		// 11	<-- a
+	printf("a : %d \n", *p++ );		// 11	<-- *p++ : (1) *p(a), (2) p++
 
+	// 프로그램 실행 시 Segmentation Fault 발생
+	// 포인터형 변수 p는 &a의 주소 값을 갖고 있지 않음.
 	printf("a : %d \n", *p );		// 쓰레기 값
-	printf("a : %d \n", ++(*p) );		// 쓰레기 값
+	printf("a : %d \n", ++(*p) );		// 쓰레기 값	<-- ++(*p) == ++a
 
 	return 0;
 }
