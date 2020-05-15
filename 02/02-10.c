ece1d0dac8037891872b2b56331b6e42a7ef21f0
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 01:00:41 +0900

    Update 02-10.c

--- 02/02-10.c
+++ 02/02-10.c
@@ -6,6 +6,10 @@ int main(void)
 
 	ch = i;					// 데이터 손실 발생(4bytes --> 1byte)
 
+	// i  --> |0000 0000|0000 0000|0000 0000|1000 0000|
+	// ch --> |1000 0000|
+
+	// ch --> unsigned char : 8bits 모두 데이터 비트(양의 정수만 표현)
 	printf("ch : %d \n", ch);		// 128
 	printf(" i : %d \n", i);		// 128
 
