3966d307a08c93283a88bdb30ccefd82dda4f29f
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 07:09:53 +0900

    Update 04-16.c

--- 04/04-16.c
+++ 04/04-16.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
+// 함수 원형 선언 : DIV 함수 -- 몫과 나머지를 구하는 함수
 int	DIV(int, int);
+
 int main(void)
 {
+	// main 함수 내부의 지역 변수 a와 b 그리고 quot, rem
 	int	a = 10, b = 3;
 	int	quot, rem;
 
@@ -12,9 +16,9 @@ int main(void)
 	return 0;
 }
 
-int	DIV(int a, int b)
+int	DIV(int a, int b)	// DIV 함수 내부의 지역 변수 a와 b
 {
-	int	quot, rem;
+	int	quot, rem;	// DIV 함수 내부의 지역 변수 quot와 rem
 
 	quot = a / b;	// 몫 계산
 	rem = a % b;	// 나머지 게산
