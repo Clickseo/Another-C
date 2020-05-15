58eb9a06e5809df9782a0a13df6ac94d62377e8f
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 00:51:55 +0900

    Update 02-07.c

--- 02/02-07.c
+++ 02/02-07.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-	int		r = 10;			// r: 원의 반지름
+	int		r = 10;			// r : 원의 반지름
 	double		res;
-	const double	PI = 3.14159;		// 기호 상수: 원주율
+	const double	PI = 3.14159;		// 기호 상수 : 원주율
 
 	res = r * r * PI;			// 314.159000
 
