7de0db0206f5b0277754af813c4da7176a3d9aac
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 05:28:28 +0900

    Update 03-03.c

--- 03/03-03.c
+++ 03/03-03.c
@@ -7,9 +7,13 @@ int main(void)
 	pa = &a;
 	pb = &b;
 
+	// &a ==  pa	
+	// &b == pb
 	printf("임의의 두 정수 입력 : ");
 	scanf_s("%d %d", pa, pb);		// scanf("%d %d", pa, pb);
 
+	// a == *pa
+	// b == *pb
 	res = *pa + *pb;			// res = a + b;
 
 	printf("%d + %d = %d \n", a, b, res);
