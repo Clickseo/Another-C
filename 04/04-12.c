3b1a30869e896e3a27e509847a65bb9bf1afeffd
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 06:58:36 +0900

    Update 04-12.c

--- 04/04-12.c
+++ 04/04-12.c
@@ -1,7 +1,13 @@
+/*
+	재귀 함수(Recursive Function)
+		- 자기 자신의 함수를 호출함으로써, 반복적인 처리를 하는 함수
+		- 주의 : 반드시 탈출(종료) 조건 명시 -- Stack Overflow 오류 발생 주의
+*/
+
 #include <stdio.h>
 
 // 함수 원형 선언 : Factorial 함수
-int  Factorial(int  n);
+int  Factorial(int  num);
 
 int main(void)
 {
@@ -10,20 +16,20 @@ int main(void)
 	printf("임의의 정수 입력 : ");
 	scanf_s("%d", &num);		// scanf("%d", &num);
 
-	// Factorial( ) 함수 호출
+	// Factorial 함수 호출
 	printf("Factorial %d : %d \n", num, Factorial(num) );
 
 	return 0;
 }
 
 // 함수 정의 : Factorial 함수
-int  Factorial(int  n)
+int  Factorial(int  num)
 {
 	// 재귀 함수의 탈출 조건
-	if(n <= 1)
+	if(num <= 1)
 		return 1;
 
 	// 재귀 함수 호출
 	// Factorial 함수 내부에서 Factorial 함수 호출
-	return  n * Factorial(n - 1);
+	return  n * Factorial(num - 1);
 }
