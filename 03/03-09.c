96d2b6f46fa43b2c2c356e640fa53b81ec5982e5
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 05:58:34 +0900

    Update 03-09.c

--- 03/03-09.c
+++ 03/03-09.c
@@ -3,13 +3,15 @@ int main(void)
 {
 	int	a = 10;
 	int	b = 20;
+	
 	const int * const pa = &a;
 
 	printf("a : %d, *pa : %d \n", a, *pa);
-	a = 50;		// 메모리 직접 접근은 가능!!!
-
-	*pa = 20;	// error: 대상체의 데이터 변경 불가!!!
-	pa = &b;	// error: 포인터 pa의 메모리 주소 변경 불가!!!
+	
+	a = 50;		// 메모리 직접 접근(a)은 가능!!!
+	
+	*pa = 20;	// (X) error : 대상체(변수 a)의 메모리 공간(데이터) 변경은 불가!!!
+	pa = &b;	// (X) error : 포인터 pa의 메모리 공간(&p) 변경은 불가(즉, 읽기만 가능)
 
 	return 0;
 }
