93c73beab0db595cfb63d7df0fc7000d022697cc
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 06:01:59 +0900

    Update 03-11.c

--- 03/03-11.c
+++ 03/03-11.c
@@ -1,3 +1,4 @@
+// 포인터 연산
 #include <stdio.h>
 int main(void)
 {
@@ -6,6 +7,7 @@ int main(void)
 
 	printf("&a : %p, pa : %p \n\n", &a, pa);
 
+	// 피연산자가 포인형 변수와 정수형 상수인 경우...
 	printf("pa + 1 : %p \n", pa + 1 );
 	printf("pa - 1 : %p \n", pa - 1 );
 
