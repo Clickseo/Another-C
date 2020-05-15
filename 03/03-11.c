f2c57591012006403d0835be6c55c0de84078c71
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 06:09:19 +0900

    Update 03-11.c

--- 03/03-11.c
+++ 03/03-11.c
@@ -7,9 +7,10 @@ int main(void)
 
 	printf("&a : %p, pa : %p \n\n", &a, pa);
 
-	// 피연산자가 포인형 변수와 정수형 상수인 경우...
-	printf("pa + 1 : %p \n", pa + 1 );
-	printf("pa - 1 : %p \n", pa - 1 );
+	// 피연산자가 포인형 변수와 정수형 상수인 경우 +1과 -1의 의미
+	// (가정) &a가 1000번지일 경우...	
+	printf("pa + 1 : %p \n", pa + 1 );	// 1004		<-- sizeof(int) * (+1) 만큼 한 칸(4bytes)이동(high address)
+	printf("pa - 1 : %p \n", pa - 1 );	//  996		<-- sizeof(int) * (-1) 만큼 한 칸(4bytes)이동(low address)
 
 	return 0;
 }
