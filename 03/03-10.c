7cae8fe5db405e229a77fa055199937d99be722f
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-16 06:01:04 +0900

    Update 03-10.c

--- 03/03-10.c
+++ 03/03-10.c
@@ -1,14 +1,16 @@
+// 포인터 연산
 #include <stdio.h>
 int main(void)
 {
-	int		a = 10, b = 20;
-	int		*pa, *pb;
+	int	a = 10, b = 20;
+	int	*pa, *pb;
 
 	pa = &a;
 	pb = &b;
 
 	printf("pa : %p, pb : %p \n\n", pa, pb);
 
+	// 2개의 피연산가 모두 포인터형 변수인 경우...
 	printf("pa + pb : %d \n", pa + pb );	// error
 	printf("pa - pb : %d \n", pa - pb );
 	printf("pa * pb : %d \n", pa * pb );	// error
