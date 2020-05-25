7b42cd5f27db5d49d567387c8531f37848a918df
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-26 01:34:26 +0900

    Update 04-23.c

--- 04/04-23.c
+++ 04/04-23.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
+
 void  SUM(int);
+
 int main(void)
 {
-	int	i;
-
-	for (i=0; i<10; i++)
+	for (int i=0; i<10; i++)
 		SUM(i);
 
 	return 0;
 }
 
-void  SUM(int num)
-{
-	// 지역 정적 변수는 처음 함수 호출 시 한 번만 초기화 된다.
-	static  int	sum = 0;
+// 지역 정적 변수는 처음 함수 호출 시 한 번만 초기화 된다.
+void  SUM(int num) {	
+	static  int	sum = 0;	// 지역정적변수 : 자동 초기화 -- 0
 
 	sum += num;
 
