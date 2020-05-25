acf9def77629b62417b3d7f23b0ae48639aee6e8
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-26 01:33:00 +0900

    Update 04-22.c

--- 04/04-22.c
+++ 04/04-22.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 void	OUTPUT(void);
 
-// 전역 변수 선언
+// 전역변수 선언
 int	a;
 
 int main(void)
 {
-	// 지역 변수 선언 및 초기화
+	// 지역변수 선언 및 초기화
 	int	a = 10;
 	printf("a : %d \n", a);		// a : 10
 
+	// 함수 호출 : OUTPUT 함수 -- 전역변수 값의 출력
 	OUTPUT();
 
 	return 0;
 }
 
-void	OUTPUT(void)
-{
-	// 전역 변수의 접근 및 출력
+void	OUTPUT(void) {	// 전역변수 값의 출력	
 	printf("a : %d \n", a);		// a : 0
 }
