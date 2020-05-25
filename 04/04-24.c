bef89cc9beab92cad6b2a1a00a4f6d7435908886
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-05-26 01:35:20 +0900

    Update 04-24.c

--- 04/04-24.c
+++ 04/04-24.c
@@ -3,15 +3,12 @@
 int main(void)
 {
 	register	int	i,  j;
+	double 	sum = 0;
 
-	double sum = 0;
-
-	for(i=0; i<=10000; i++)
-	{
+	for(i=0; i<=10000; i++) {
 		for(j=0; j<=10000; j++)
 			sum += i * j;
 	}
-
 	printf("sum = %f \n", sum);
 
 	return 0;
