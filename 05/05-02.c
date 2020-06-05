ee1ff9264e1368413b0328db60b9d761d49b322e
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-05 16:25:05 +0900

    Update 05-02.c

--- 05/05-02.c
+++ 05/05-02.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-// 매크로 상수 : 배열의 원소 개수
-#define MAX_SIZE 3
+// 매크로 상수: 배열의 원소 개수
+#define arrMAXSIZE 3
 
 int main(void)
 {
-	// 배열 선언 : 메모리 할당
-	int	arr[MAX_SIZE];			// int	arr[3];
+	// 배열 선언: 메모리 할당
+	int	arr[arrMAXSIZE];		// int	arr[3];
 
-	// 사용자로부터 3개의 정수형 데이터 입력
-	for(int i=0; i<MAX_SIZE; i++)
+	// 사용자로부터 임의의 정수 3개에 대하여 입력 처리
+	for(int i=0; i<arrMAXSIZE; i++)
 		scanf_s("%d", &arr[i] );	// scanf("%d", &arr[i] );
 
 	// 배열에 저장되어 있는 데이터 출력
-	for(i=0; i<MAX_SIZE; i++)
+	for(int i=0; i<arrMAXSIZE; i++)
 		printf("arr[%d]: %d \n", i, arr[i] );
 
 	return 0;
