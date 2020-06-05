919b410af750741833fac31ba8b7a66fb7e2d17f
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-05 16:34:24 +0900

    Update 05-03.c

--- 05/05-03.c
+++ 05/05-03.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-#define MAX_SIZE 10
+#define arrMAXSIZE 10
 
 int main(void)
 {
-	int	arr[MAX_SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int	arr[arrMAXSIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-	// 배열의 순방향 출력
-	for(int i=0; i<MAX_SIZE; i++)
+	// 배열의 순방향 출력: arr[0] --> arr[9]
+	for(int i=0; i<arrMAXSIZE; i++)
 		printf("%3d", arr[i]);
 	printf("\n");
 
-	// 배열의 역방향 출력
-	for(int i=MAX_SIZE-1; i>=0; i--)
+	// 배열의 역방향 출력: arr[9] --> arr[0]
+	for(int i=arrMAXSIZE-1; i>=0; i--)
 		printf("%3d", arr[i]);
 	printf("\n");
 
