ba66d1033d5fe643023be162e5e3bd2b4defa505
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 02:10:25 +0900

    Update 05-16.c

--- 05/05-16.c
+++ 05/05-16.c
@@ -1,21 +1,19 @@
 // 삽입 정렬 알고리즘
-void  Insertion_Sort(int *pArr, int num)
+void  insertionSort(int *pArr, int num)
 {
-	int	temp;
+	int	i, j, temp;
 
-	for(int i = 1; i < num; i++)
-	{
+	for(i=1; i<num; i++) {
 		// 새로 삽입할 데이터를 임시 변수 temp에 저장
-		temp = *(pArr + i);
+		temp = *(pArr+i);
 
 		// 정렬된 영역에서 temp 가 들어갈 공간 확보
 		// 삽입할 데이터보다 큰 원소를 한 칸씩 오른쪽으로 이동
-		for(int j = i - 1; j >=0  &&  *(pArr + j) > temp; j--)
-			*(pArr + j + 1)  =  *(pArr + j);
+		for(j=i-1; j>=0 && *(pArr+j)>temp; j--)
+			*(pArr+j+1) = *(pArr+j);
 
 		// 확보된 공간에 새로운 데이터를 삽입
-		*(pArr + j + 1)  =  temp;
+		*(pArr+j+1) = temp;
 	}
-
 	return;
 }
