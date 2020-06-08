c8b5ba12124a982f6b6441c8596a46abeb8dbb4a
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 01:54:11 +0900

    Update 05-12.c

--- 05/05-12.c
+++ 05/05-12.c
@@ -1,25 +1,24 @@
+// 배열과 함수: 전체 배열 전달
 #include <stdio.h>
 
-#define MAX_SIZE 3
+#define arrMAXSIZE 3
 
-// 함수 원형 : 전체 배열 원소를 출력하기 위한 OUTPUT 함수
+// 함수 원형: 전체 배열 원소를 출력하기 위한 OUTPUT 함수
 void	OUTPUT(int *, int);
 
 int main(void)
 {
-	int	arr[3] = { 10, 20, 30 };
+	int	arr[arrMAXSIZE] = { 10, 20, 30 };
 
-	// 함수 호출 : 배열 전체 원소 출력을 위한 OUTPUT 함수
-	OUTPUT(arr, MAX_SIZE);
+	// 함수 호출: 배열 전체 원소 출력을 위한 OUTPUT 함수
+	OUTPUT(arr, arrMAXSIZE);
 
 	return 0;
 }
 
-// 함수 정의 : 전체 배열 원소를 출력하기 위한 OUTPUT 함수
-void	OUTPUT(int *p, int n)
-{
-	for(int i=0; i<n; i++)
+// 함수 정의: 전체 배열 원소를 출력하기 위한 OUTPUT 함수
+void	OUTPUT(int *p, int num) {
+	for(int i=0; i<num; i++)
 		printf("arr[%d]: %d \n", i, *(p+i) );
-
 	return;
 }
