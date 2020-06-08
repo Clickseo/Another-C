9011c509bc6632bd58735e0a1c8e6578eb8b9091
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 01:41:33 +0900

    Update 05-08.c

--- 05/05-08.c
+++ 05/05-08.c
@@ -1,21 +1,20 @@
+// 배열과 포인터: 배열 원소의 순방향과 역방향 출력
 #include <stdio.h>
 
-#define MAX_SIZE 5
+#define arrMAXSIZE 5
 
 int main(void)
 {
-	int	arr[MAX_SIZE] = { 10, 20, 30, 40, 50 };
-	int	*p;
+	int	arr[arrMAXSIZE] = { 10, 20, 30, 40, 50 };
+	int*	p = arr;			// int* p = &arr[0];	
 
-
-	printf("순방향 출력 : ");
-	p = arr;				// p = &arr[0];	
-	for ( ; p < arr + MAX_SIZE;  p++ )
+	printf("순방향 출력: ");
+	for ( ; p < arr + arrMAXSIZE;  p++ )
 		printf("%3d", *p );
 	printf("\n");
 
-	printf("역방향 출력 : ");	
-	p = arr + MAX_SIZE - 1;			// p = &arr[4];
+	printf("역방향 출력: ");	
+	p = arr + arrMAXSIZE - 1;		// p = &arr[4];
 	for ( ; p >= arr;  p-- )
 		printf("%3d", *p );
 	printf("\n");
