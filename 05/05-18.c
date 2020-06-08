899843e0ed105961b66ab5313b041f372392e882
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 02:33:42 +0900

    Update 05-18.c

--- 05/05-18.c
+++ 05/05-18.c
@@ -1,33 +1,35 @@
+// 이진 탐색 알고리즘
+//	1) 정렬된 데이터에 대하여 탐색 수행
+//	2) 중복된 데이터가 존재하지 않을 경우 탐색 수행
 #include <stdio.h>
+#include <stdbool.h>	// bool, true, false
 
-#define MAX_SIZE 10
+#define arrMAXSIZE 10
 
 // 함수 원형 선언
-int	*BinarySearch(int *, int *, int *, int);	// 1) 이진 탐색 알고리즘
+int	*binarySearch(int *, int *, int *, int);	// 1) 이진 탐색 알고리즘
 void 	OUTPUT(int *pArr, int num);			// 2) 전체 배열 출력
 
 int main(void)
 {
 	int	num, *pLoc;
-	int	arr[MAX_SIZE] = { 10, 20, 30, 40, 50,
-					60, 70, 80, 90, 99};
+	int	arr[arrMAXSIZE] = { 10, 20, 30, 40, 50,	60, 70, 80, 90, 99};
 
-	// 배열의 원본 데이터 출력
+	// 원본 데이터 출력
 	printf("데이터 : ");
-	OUTPUT(arr, MAX_SIZE);
+	OUTPUT(arr, arrMAXSIZE);
 
-	while(1)
+	while(true)
 	{
 		printf("\n찾고자 하는 데이터 (검색 종료: 0): ");
 		scanf_s("%d", &num);		// scanf("%d", &num);
-		fflush(stdin);
 
 		// 사용자가 0 을 입력하면 검색 종료!!!
 		if(num == 0)
 			break;
 
 		// 함수 호출 : 데이터 검색을 위한 이진 탐색 함수
-		pLoc = BinarySearch(arr, arr, arr+MAX_SIZE-1, num);
+		pLoc = binarySearch(arr, arr, arr+arrMAXSIZE-1, num);
 
 		// 탐색 실패 : BinarySearch 함수의 반환 값이 NULL
 		if(pLoc == NULL)	printf("존재하지 않는 데이터!!! \n");
@@ -37,32 +39,27 @@ int main(void)
 	return 0;
 }
 
-// 이진 탐색 알고리즘 : num 개의 배열원소에서 key 값을 검색
-int  *BinarySearch(int *pArr, int *pBegin, int *pEnd, int key)
-{
-	int	*pMid, *pFirst = pBegin, *pLast = pEnd;
-
-	// first > last 이면 검색 종료: 검색 실패
-	while(pFirst <= pLast)
-	{
-		// first ~ last 사이의 중간 원소의 위치(주소) 계산
-		pMid = pFirst + (pLast - pFirst) / 2;
+// 이진 탐색 알고리즘: pBegin ~ pEnd 사이의 배열 원소들 중에서 key 값을 검색
+int  *binarySearch(int *pArr, int *pBegin, int *pEnd, int key) {	
+	int*	pFirst = pBegin;
+	int*	pLast = pEnd;
+	int*	pMid;
+	while(pFirst <= pLast)	{			// first > last 이면 검색 종료: 검색 실패		
+		pMid = pFirst + (pLast-pFirst) / 2;	// first ~ last 사이의 중간 원소의 위치(주소) 계산
 
 		if(*pMid == key)		return  pMid;
 		else if(*pMid > key)		pLast = pMid - 1;
 		else if(*pMid < key)		pFirst = pMid + 1;
 	}
 
-	// 검색 실패할 경우 NULL 반환
+	// 검색 실패할 경우 NULL 값 반환
 	return  NULL;
 }
 
-// 배열 전체 원소 출력 : num 개의 배열 원소 전체 출력
-void OUTPUT(int *pArr, int num)
-{
-	for(int i = 0; i < num; i++)
-		printf("%3d", *(pArr + i));
+// 배열 전체 원소(데이터) 출력
+void OUTPUT(int *pArr, int num) {
+	for(int i=0; i<num; i++)
+		printf("%3d", *(pArr+i));
 	printf("\n");
-
 	return;
 }
