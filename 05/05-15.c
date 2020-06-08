1c8bea25eee14e479867cff7a6730d875dcd3e6f
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 02:08:10 +0900

    Update 05-15.c

--- 05/05-15.c
+++ 05/05-15.c
@@ -1,12 +1,9 @@
 // 버블 정렬 알고리즘
-void  Bubble_Sort(int *pArr, int num)
-{
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = n - 1; j > i; j --)
-			if(*(pArr + j) < *(pArr + j - 1))
-				SWAP(pArr + j, pArr + j - 1);
+void  bubbleSort(int *pArr, int num) {
+	for(int i=0; i<num; i++) {
+		for(int j=num-1; j>i; j --)
+			if(*(pArr+j) < *(pArr+j-1))
+				SWAP(pArr+j, pArr+j-1);
 	}
-
 	return;
 }
