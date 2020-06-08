19d41819e1c8101e6eae98af1704fe6dda676889
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 01:48:46 +0900

    Update 05-10.c

--- 05/05-10.c
+++ 05/05-10.c
@@ -1,23 +1,23 @@
+// 배열과 함수: 개별 원소 전달
 #include <stdio.h>
 
-#define MAX_SIZE 3
+#define arrMAXSIZE 5
 
-// 함수 원형 : 정수 데이터 하나를 출력하는 OUTPUT 함수
+// 함수 원형: 정수 데이터 하나를 출력하는 OUTPUT 함수
 void	OUTPUT(int);
 
 int main(void)
 {
-	int	arr[3] = { 10, 20, 30 };
+	int	arr[arrMAXSIZE] = { 10, 20, 30 };
 
 	// 배열의 원소 개수만큼 OUTPUT 함수 호출을 반복
-	for(int i=0; i<MAX_SIZE; i++)
+	for(int i=0; i<arrMAXSIZE; i++)
 		OUTPUT(arr[i]);
 
 	return 0;
 }
 
-// 함수 정의 : 정수 데이터 하나를 출력하는 OUTPUT 함수
-void	OUTPUT(int temp)
-{
+// 함수 정의: 정수 데이터 하나를 출력하는 OUTPUT 함수
+void	OUTPUT(int temp) {
 	printf("temp: %d \n", temp );
 }
