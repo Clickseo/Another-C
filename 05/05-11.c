a584092b215bcc78cb38ccab1454101c6cac0fa5
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 01:50:29 +0900

    Update 05-11.c

--- 05/05-11.c
+++ 05/05-11.c
@@ -1,21 +1,22 @@
+// 배열과 함수: 개별 원소 전달
 #include <stdio.h>
-#define MAX_SIZE 3
+
+#define arrMAXSIZE 3
 
 // 함수 원형 : 3개의 배열 원소를 출력하기 위한 OUTPUT 함수
 void	OUTPUT(int, int, int);
 
 int main(void)
 {
-	int	arr[3] = {10, 20, 30};
+	int	arr[arrMAXSIZE] = {10, 20, 30};
 
-	// 함수 호출 : 3개의 배열 원소를 함수 인자로 넘겨 OUTPUT 함수 호출
+	// 함수 호출: 3개의 배열 원소를 함수 인자로 넘겨 OUTPUT 함수 호출
 	OUTPUT(arr[0], arr[1], arr[2]);
 
 	return 0;
 }
 
-// 함수 정의 : 3개의 변수를 한꺼번에 출력하는 OUTPUT 함수
-void	OUTPUT(int a, int b, int c)
-{
+// 함수 정의: 3개의 변수를 출력하는 OUTPUT 함수
+void	OUTPUT(int a, int b, int c) {
 	printf("a: %d, b: %d, c: %d \n", a, b, c);
 }
