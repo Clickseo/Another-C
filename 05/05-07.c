bfc8441fd6d02d58799d612f35973d4b9b6b423a
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 01:38:36 +0900

    Update 05-07.c

--- 05/05-07.c
+++ 05/05-07.c
@@ -1,15 +1,17 @@
+// 배열과 포인터
 #include <stdio.h>
 
-#define MAX_SIZE 5
+#define arrMAXSIZE 5
 
 int main(void)
 {
-	int	arr[MAX_SIZE] = {10, 20, 30, 40, 50};
+	int	arr[arrMAXSIZE] = {10, 20, 30, 40, 50};
 	int	*p;
 
 	p = arr;		// p = &arr[0];
 
-	for(int i=0; i<MAX_SIZE; i++)
+	// arr[i] == p[i] == *(p+i)
+	for(int i=0; i<arrMAXSIZE; i++)
 		printf("arr[%d]: %d, %d \n", i, arr[i], *(p+i));
 
 	return 0;
