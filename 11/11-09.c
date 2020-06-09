a1e466fd8e1fd703deef8da06708b0c222ce0ec4
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 16:12:05 +0900

    Update 11-09.c

--- 11/11-09.c
+++ 11/11-09.c
@@ -1,12 +1,14 @@
+// 함수 포인터 : 직접 호출과 간접 호출
 #include <stdio.h>
 int main(void)
 {
 	int	(*p)(void) = main;
 
 	printf("main : %p \n", main);
-	printf("p     : %p \n", p);
+	printf("p    : %p \n", p);
 
-	p();		// main();
+	// main();			// 직접 호출
+	p();				// 간접 호출
 
 	return 0;
 }
