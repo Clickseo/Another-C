08c62f7e8314e9ce5babd5bfa37126310ba6f5ff
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 16:13:19 +0900

    Update 11-10.c

--- 11/11-10.c
+++ 11/11-10.c
@@ -1,3 +1,4 @@
+// 함수 포인터
 #include <stdio.h>
 
 int ADD(int, int);
@@ -8,19 +9,19 @@ int DIV(int, int);
 int main(void)
 {
 	int	a = 10, b = 20;
-	int	(*p)(int, int);
+	int	(*p)(int, int);		// 함수 포인터
 
 	p = ADD;
-	printf("%d + %d = %d \n", a, b, p(10, 20));  // ADD(10, 20)
+	printf("%d + %d = %d \n", a, b, p(10, 20));  	// ADD(10, 20)
 
 	p = SUB;
-	printf("%d + %d = %d \n", a, b, p(10, 20));  // SUB(10, 20)
+	printf("%d + %d = %d \n", a, b, p(10, 20));  	// SUB(10, 20)
 
 	p = MUL;
-	printf("%d + %d = %d \n", a, b, p(10, 20));  // MUL(10, 20)
+	printf("%d + %d = %d \n", a, b, p(10, 20));  	// MUL(10, 20)
 
 	p = DIV;
-	printf("%d / %d = %d \n", a, b, p(10, 20));  // DIV(10, 20)
+	printf("%d / %d = %d \n", a, b, p(10, 20));  	// DIV(10, 20)
 
 	return 0;
 }
