c111da3db29bc31cb002918a6a7326c78560ec87
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 15:47:43 +0900

    Update 11-02.c

--- 11/11-02.c
+++ 11/11-02.c
@@ -1,18 +1,20 @@
+// 다중 포인터
 #include <stdio.h>
 int main(void)
 {
+	// pp --> p --> a
 	int		a;
 	int		*p = &a;
 	int		**pp = &p;
 
-	scanf("%d", &a);
-	printf("a    : %d \n", a);
+	scanf_s("%d", &a);
+	printf("a : %d \n", a);
 
-	scanf("%d", p);			// scanf("%d", &a);
-	printf("a    : %d \n", a);
+	scanf_s("%d", p);		// scanf_s("%d", &a);
+	printf("a : %d \n", a);
 
-	scanf("%d", *pp);			// scanf("%d", &a);
-	printf("a    : %d \n", a);
+	scanf_s("%d", *pp);		// scanf_s("%d", &a);
+	printf("a : %d \n", a);
 
 	return 0;
 }
