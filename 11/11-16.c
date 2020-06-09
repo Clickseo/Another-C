c2aa326d462ec6af4f6d912f45b743fe2844418c
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 16:24:04 +0900

    Update 11-16.c

--- 11/11-16.c
+++ 11/11-16.c
@@ -1,13 +1,15 @@
+// 2차원 배열 : 포인터와 함수
 #include <stdio.h>
 
-#define ROW 3
-#define COL 5
+#define ROW 3		// 행
+#define COL 5		// 열
 
 void INPUT(int (*)[COL], int, int);
 void OUTPUT(int (*)[COL], int, int);
 
 int main(void)
 {
+	// 3행 5열의 2차원 배열 선언
 	int	table[3][5];
 
 	INPUT(table, ROW, COL);
@@ -16,33 +18,21 @@ int main(void)
 	return 0;
 }
 
-void INPUT(int (*p)[COL], int row, int col)
-{
-	int	i, j;
-
-	for(i=0; i<row; i++)
-	{
+void INPUT(int (*p)[COL], int row, int col) {
+	for(int i=0; i<row; i++) {
 		printf("%3d : ", i+1);
-
-		for(j=0; j<col; j++)
-			scanf("%d", *(p+i)+j );
+		for(int j=0; j<col; j++)
+			scanf_s("%d", *(p+i)+j );
 	}
-
 	return;
 }
 
-void OUTPUT(int (*p)[COL], int row, int col)
-{
-	int	i, j;
-
-	for(i=0; i<row; i++)
-	{
+void OUTPUT(int (*p)[COL], int row, int col) {
+	for(int i=0; i<row; i++) {
 		printf("%3d : ", i+1);
-
-		for(j=0; j<col; j++)
+		for(int j=0; j<col; j++)
 			printf("%3d", *(*(p+i)+j) );
 		printf("\n");
-	}
-
+	}	
 	return;
 }
