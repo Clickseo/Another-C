e4ec3cd3ef4d8918fd5126c00aff7d9f0ccac6e6
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 16:17:51 +0900

    Update 11-12.c

--- 11/11-12.c
+++ 11/11-12.c
@@ -1,3 +1,4 @@
+// í•¨ìˆ˜ í¬ì¸í„°
 #include <stdio.h>
 
 int ADD(int, int);
@@ -9,35 +10,32 @@ void OUTPUT(int (*)(int, int));
 
 int main(void)
 {
-	int		num;
+	int	num;
 
-	printf("### »çÄ¢¿¬»ê ### \n\n");
-	printf("1) µ¡  ¼À ¿¬»ê \n");
-	printf("2) »¬  ¼À ¿¬»ê \n");
-	printf("3) °ö  ¼À ¿¬»ê \n");
-	printf("4) ³ª´°¼À ¿¬»ê \n\n");
+	printf("### ì‚¬ì¹™ì—°ì‚° ### \n\n");
+	printf("1) ë§  ì…ˆ ì—°ì‚° \n");
+	printf("2) ëº„  ì…ˆ ì—°ì‚° \n");
+	printf("3) ê³±  ì…ˆ ì—°ì‚° \n");
+	printf("4) ë‚˜ëˆ—ì…ˆ ì—°ì‚° \n\n");
 
-	printf("¿¬»êÀÇ Á¾·ù ¼±ÅÃ : ");
-	scanf("%d", &num);
+	printf("ì—°ì‚°ì˜ ì¢…ë¥˜ ì„ íƒ : ");
+	scanf_s("%d", &num);
 
-	switch(num)
-	{
-		case 1: OUTPUT(ADD);		break;
-		case 2: OUTPUT(SUB);		break;
-		case 3: OUTPUT(MUL);		break;
-		case 4: OUTPUT(DIV);		break;
-		default: printf("Àß¸øµÈ ¼±ÅÃ!!! \n");
+	switch(num) {
+		case 1: OUTPUT(ADD);	break;
+		case 2: OUTPUT(SUB);	break;
+		case 3: OUTPUT(MUL);	break;
+		case 4: OUTPUT(DIV);	break;
+		default: printf("ì˜ëª»ëœ ì„ íƒ!!! \n");
 			  return 0;
 	}
 
 	return 0;
 }
 
-void OUTPUT(int (*p)(int, int))
-{
+void OUTPUT(int (*p)(int, int)) {
 	int		a = 10, b = 20;
-
-	printf("¿¬»ê °á°ú : %d \n", p(a, b));
+	printf("ì—°ì‚° ê²°ê³¼ : %d \n", p(a, b));
 }
 
 int ADD(int a, int b)	{	return a + b;	}
