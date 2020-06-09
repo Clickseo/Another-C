a8939675faafd8392be16d192e24f453314744cb
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-06-09 15:45:42 +0900

    Update 11-01.c

--- 11/11-01.c
+++ 11/11-01.c
@@ -1,11 +1,13 @@
+// 다중 포인터
 #include <stdio.h>
 int main(void)
 {
+	// pp --> p --> a
 	int	a = 10;
 	int	*p = &a;
 	int	**pp = &p;
 
-	printf("a    : %d \n", a);			// 10
+	printf("a    : %d \n", a);		// 10
 	printf("*p   : %d \n", *p);		// 10
 	printf("**pp : %d \n", **pp);		// 10
 
