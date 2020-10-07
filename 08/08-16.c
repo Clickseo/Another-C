4e572621efc047ad14cb4b83f04e666b895f9b2d
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-08 01:06:27 +0900

    Update 08-16.c

--- 08/08-16.c
+++ 08/08-16.c
@@ -1,15 +1,14 @@
+// 구조체와 공용체
 #include <stdio.h>
 
-typedef struct _uint
-{
-	unsigned int	FirstBit  : 8;
-	unsigned int	SecondBit : 8;
-	unsigned int	ThirdBit  : 8;
-	unsigned int	ForthBit  : 8;
+typedef struct _uint {
+	unsigned int	FirstBit: 8;
+	unsigned int	SecondBit: 8;
+	unsigned int	ThirdBit: 8;
+	unsigned int	ForthBit: 8;
 }UINT;
 
-typedef union _endian
-{
+typedef union _endian {
 	int	a;
 	UINT	b;
 }ENDIAN;
@@ -20,11 +19,12 @@ int main(void)
 
 	temp.a = 0x12345678;
 
-	printf("temp.i : %X \n", temp.a);		// 12345678
-	printf("temp.i : %X \n", temp.b.FirstBit);	// 78
-	printf("temp.i : %X \n", temp.b.SecondBit);	// 56
-	printf("temp.i : %X \n", temp.b.ThirdBit);	// 34
-	printf("temp.i : %X \n", temp.b.ForthBit);	// 12
+	// "Big endian" 과 "Little endian"
+	printf("temp.i: %X \n", temp.a);		// 12345678
+	printf("temp.i: %X \n", temp.b.FirstBit);	// 78
+	printf("temp.i: %X \n", temp.b.SecondBit);	// 56
+	printf("temp.i: %X \n", temp.b.ThirdBit);	// 34
+	printf("temp.i: %X \n", temp.b.ForthBit);	// 12
 
 	return 0;
 }
