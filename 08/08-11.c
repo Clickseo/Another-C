2279fce4adc3cf8853ffa5e7a2a27756688ce5f8
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-07 23:48:18 +0900

    Update 08-11.c

--- 08/08-11.c
+++ 08/08-11.c
@@ -1,22 +1,23 @@
+// 구조체형 변수 선언 및 정의
 #include <stdio.h>
 
-typedef struct _point
-{
+// 형식(재) 정의된 구조체: POINT
+typedef struct _point {
 	int x;
 	int y;
 }POINT;
 
 int main(void)
 {
-	struct _point a = {10, 20};
-
-	POINT *p = &a;
+	// 구조체형 변수 선언 및 초기화
+	struct _point	a = { 10, 20 };
+	POINT			*pS = &a;
 
 	a.x = 10;
-	a->x = 10;
-	p->x = 10;
-	(*p).x = 10;
-	p.x = 10;
+	a->x = 10;		// Error
+	pS->x = 10;
+	(*pS).x = 10;
+	pS.x = 10;		// Error
 
 	return 0;
 }
