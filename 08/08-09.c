d3b01d47e048ff28f61f42115e1b1d11c23af621
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-07 23:44:52 +0900

    Update 08-09.c

--- 08/08-09.c
+++ 08/08-09.c
@@ -1,4 +1,5 @@
-// 구조체와 포인터: 구조체형 변수의 간접 접근 방법
+// 구조체와 포인터
+// 구조체형 변수의 간접 접근 방법: 포인터형 변수
 #include <stdio.h>
 
 // 형식(재) 정의된 구조체: SCORE
@@ -13,10 +14,10 @@ int main(void)
 	SCORE	s;		// 구조체형 변수
 	SCORE	*pS = &s;	// 포인터형 변수
 
-	printf("이름: ");	gets_s((*pS).name, sizeof((*pS).name));	// gets((*pS).name);
-	printf("국어: ");	scanf_s("%d", &(*pS).kor);				// scanf("%d", &(*pS).kor);
-	printf("영어: ");	scanf_s("%d", &(*pS).eng);				// scanf("%d", &(*pS).eng);
-	printf("수학: ");	scanf_s("%d", &(*pS).math);				// scanf("%d", &(*pS).math);
+	printf("이름: ");	gets_s((*pS).name, sizeof((*pS).name));		// gets((*pS).name);
+	printf("국어: ");	scanf_s("%d", &(*pS).kor);			// scanf("%d", &(*pS).kor);
+	printf("영어: ");	scanf_s("%d", &(*pS).eng);			// scanf("%d", &(*pS).eng);
+	printf("수학: ");	scanf_s("%d", &(*pS).math);			// scanf("%d", &(*pS).math);
 
 	(*pS).tot = (*pS).kor + (*pS).eng + (*pS).math;
 	(*pS).ave = (float)((*pS).kor + (*pS).eng + (*pS).math) / 3;
