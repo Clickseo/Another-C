b9b397237b0c195a3182deaaca50a37d14523c07
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-08 00:52:04 +0900

    Update 08-12.c

--- 08/08-12.c
+++ 08/08-12.c
@@ -5,33 +5,33 @@
 // 형식(재) 정의된 구조체: SCORE
 typedef struct _score {
 	char	name[12];
-	int	kor, eng, math, tot;
+	int		kor, eng, math, tot;
 	float	ave;
 }SCORE;
 
 // 
-void OUTPUT(const char* pName,	int pKor, int pEng, int pMath, int pTot, float pAve);
+void OUTPUT(const char* pName, int pKor, int pEng, int pMath, int pTot, float pAve);
 
 int main(void)
 {
-	SCORE	a;
+	SCORE	s;
 
-	printf("이름: ");	gets_s(a.name, sizeof(a.name));		// gets(a.name);
-	printf("국어: ");	scanf_s("%d", &a.kor);			// scanf("%d", &a.kor);
-	printf("영어: ");	scanf_s("%d", &a.eng);			// scanf("%d", &a.eng);
-	printf("수학: ");	scanf_s("%d", &a.math);			// scanf("%d", &a.math);
+	printf("이름: ");	gets_s(s.name, sizeof(s.name));		// gets(s.name);
+	printf("국어: ");	scanf_s("%d", &s.kor);			// scanf("%d", &s.kor);
+	printf("영어: ");	scanf_s("%d", &s.eng);			// scanf("%d", &s.eng);
+	printf("수학: ");	scanf_s("%d", &s.math);			// scanf("%d", &s.math);
 
-	a.tot = a.kor + a.eng + a.math;
-	a.ave = (float)(a.kor + a.eng + a.math) / 3;
+	s.tot = s.kor + s.eng + s.math;
+	s.ave = (float)(s.kor + s.eng + s.math) / 3;
 
 	// 함수 호출: 구조체 멤버를 개별적으로 함수에 복사본 전달
 	// 값에 의한 호출(pass by Value)
-	OUTPUT(a.name, a.kor, a.eng, a.math, a.tot, a.ave);
+	OUTPUT(s.name, s.kor, s.eng, s.math, s.tot, s.ave);
 
 	return 0;
 }
 
-void OUTPUT(const char	*pName,	int pKor, int pEng, int pMath,	int pTot, float pAve)
+void OUTPUT(const char* pName, int pKor, int pEng, int pMath, int pTot, float pAve)
 {
 	printf("\n %10s 학생의 성적 결과 \n", pName);
 	printf("국어: %3d, 영어: %3d, 수학:  %3d\n", pKor, pEng, pMath);
