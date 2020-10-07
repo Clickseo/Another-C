a00307f3bf6b76530e812ffebebcab66e0a81711
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-08 00:50:26 +0900

    Update 08-13.c

--- 08/08-13.c
+++ 08/08-13.c
@@ -5,7 +5,7 @@
 // 형식(재) 정의된 구조체: SCORE
 typedef struct _score {
 	char	name[12];
-	int	kor, eng, math, tot;
+	int		kor, eng, math, tot;
 	float	ave;
 }SCORE;
 
@@ -13,28 +13,28 @@ void OUTPUT(SCORE);
 
 int main(void)
 {
-	SCORE	a;
+	SCORE	s;
 
-	printf("이름: ");	gets_s(a.name, sizeof(a.name));		// gets(a.name);
-	printf("국어: ");	scanf_s("%d", &a.kor);			// scanf("%d", &a.kor);
-	printf("영어: ");	scanf_s("%d", &a.eng);			// scanf("%d", &a.eng);
-	printf("수학: ");	scanf_s("%d", &a.math);			// scanf("%d", &a.math);
+	printf("이름: ");	gets_s(s.name, sizeof(s.name));		// gets(s.name);
+	printf("국어: ");	scanf_s("%d", &s.kor);			// scanf("%d", &s.kor);
+	printf("영어: ");	scanf_s("%d", &s.eng);			// scanf("%d", &s.eng);
+	printf("수학: ");	scanf_s("%d", &s.math);			// scanf("%d", &s.math);
 
-	a.tot = a.kor + a.eng + a.math;
-	a.ave = (float)(a.kor + a.eng + a.math) / 3;
+	s.tot = s.kor + s.eng + s.math;
+	s.ave = (float)(s.kor + s.eng + s.math) / 3;
 
 	// 함수 호출: 구조체형 변수의 복사본 전달
 	// 값에 의한 전달(pass by Value)
-	OUTPUT(a);
+	OUTPUT(s);
 
 	return 0;
 }
 
-// 함수 정의: temp = a;
+// 함수 정의: temp = s;
 void OUTPUT(SCORE temp)
 {
 	printf("\n %10s 학생의 성적 결과 \n", temp.name);
-	printf("국어: %3d, 영어: %3d, 수학:  %3d \n",	temp.kor, temp.eng, temp.math);
+	printf("국어: %3d, 영어: %3d, 수학:  %3d \n", temp.kor, temp.eng, temp.math);
 	printf("총점: %3d, 평균: %8.2lf \n", temp.tot, temp.ave);
 
 	return;
