5d67879ebb9ace5ff6251057b55e001c7b52b574
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-31 01:37:05 +0900

    Update 09-19.c

--- 09/09-19.c
+++ 09/09-19.c
@@ -1,8 +1,10 @@
-// input.c <-- SCORE 형식 정의 추가
+// input.c <-- SCORE 형식 정의 매크로 상수 추가
 #include <stdio.h>
 
 // 매크로 상수: 학생 수
 #define stuMAXSIZE 3
+
+// 형식(재) 정의된 구조체: SCORE
 typedef  struct  _score {
 	char	name[12];
 	int	kor, eng, math, tot;
