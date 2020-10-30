a42f541b156556026c498c4ff3bdc2f8c0aa213e
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-31 00:32:18 +0900

    Update 09-04.c

--- 09/09-04.c
+++ 09/09-04.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+
+// 함수 원형: 일반 함수
 int	ADD(int, int);
+
 int main(void)
 {
 	int	res;
@@ -11,4 +14,4 @@ int main(void)
 	return 0;
 }
 
-int	ADD(int a, int b)	{	return a + b;	}
+int	ADD(int a, int b) { return a + b; }
