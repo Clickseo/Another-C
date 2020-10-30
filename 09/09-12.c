4166f0d1264577c14276a7862f7defc25647eb91
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-31 00:42:00 +0900

    Update 09-12.c

--- 09/09-12.c
+++ 09/09-12.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+
+// 첫 번째 프로그램
 int main(void)
 {
 	printf("Hello World!!! \n");
-
 	return 0;
 }
 
-/*
+/* 두 번째 프로그램
 int main(void)
 {
 	printf("Hi~ Clickseo!!! \n");
-
 	return 0;
 }
 */
