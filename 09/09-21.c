be1427e57d4aaf7b25ffbd1d6dbf8ba1c734abf9
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-31 01:39:03 +0900

    Update 09-21.c

--- 09/09-21.c
+++ 09/09-21.c
@@ -1,11 +1,15 @@
+// 헤더 파일: score.h
+
+// 매크로 상수: 학생 수
 #define MAX_SIZE 3
 
-typedef  struct  _score
-{
+// 형식(재) 정의된 구조체: SCORE
+typedef  struct  _score {
 	char	name[12];
 	int	kor, eng, math, tot;
 	float	ave;
 } SCORE;
 
-void	INPUT(SCORE  *);
-void	OUTPUT(SCORE  *);
+// 함수 원형: 학생 정보 입출력 함수
+void	INPUT(SCORE *pArr, int num);
+void	OUTPUT(SCORE *pArr, int num);
