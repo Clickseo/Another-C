f32928b1ecacce24ab754073773c761d076ee741
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-31 00:34:56 +0900

    Update 09-07.c

--- 09/09-07.c
+++ 09/09-07.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
+// 함수 원형: 일반 함수
 int	DIV(int, int);
+
 int main(void)
 {
 	double		res;
 
+	// 데이터 손실 발생
 	res = DIV(10.0, 3.0);
 
 	printf("res = %.1f \n", res);	// res = 3.0
@@ -11,4 +15,4 @@ int main(void)
 	return 0;
 }
 
-int	DIV(int a, int b)	{	return a / b;		}
+int	DIV(int a, int b) { return a / b; }
