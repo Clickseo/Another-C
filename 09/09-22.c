a90eb8af2fc1fd00285039adb7eb232ab1dfb8c3
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-31 01:41:19 +0900

    Update 09-22.c

--- 09/09-22.c
+++ 09/09-22.c
@@ -1,13 +1,19 @@
-#define MAX_SIZE 3
+// 헤더 파일: score.h
+
+#ifndef stuMAXSIZE
+	#define stuMAXSIZE 3
+#endif
+
+// 조건 컴파일: _SCORE_H_ 매크로의 정의 여부에 따른 매크로 정의
 #ifndef _SCORE_H_
 #define _SCORE_H_
-typedef  struct  _score
-{
+typedef  struct  _score {
 	char	name[12];
 	int	kor, eng, math, tot;
 	float	ave;
 } SCORE;
 #endif
 
-void	INPUT(SCORE  *);
-void	OUTPUT(SCORE  *);
+// 함수 원형: 학생 성적 입출력 함수
+void	INPUT(SCORE *pArr, int num);
+void	OUTPUT(SCORE *pArr, int num);
