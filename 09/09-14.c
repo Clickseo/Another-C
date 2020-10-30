53a9ba49ceebb6f708fc4d9dc100ccf72acf004c
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2020-10-31 00:44:49 +0900

    Update 09-14.c

--- 09/09-14.c
+++ 09/09-14.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-// ��ũ�� ����
+// 매크로 이름 지정
 #define HELLO
 
+// 첫 번째 프로그램
 #ifdef HELLO			// #if defined HELLO
 int main(void)
 {
 	printf("Hello World!!! \n");
-
 	return 0;
 }
+// 두 번째 프로그램
 #else
 int main(void)
 {
 	printf("Hi~ Clickseo!!! \n");
-
 	return 0;
 }
 #endif
