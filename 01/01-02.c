e1cc4b1e4a185ea9c529aefdf40b11cc6b309674
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2021-03-06 19:00:10 +0900

    Update 01-02.c

--- 01/01-02.c
+++ 01/01-02.c
@@ -3,12 +3,12 @@ int main(void)
 {
 	int	a;
 
-	// 사용자로부터 데이터를 입력 받음
-	printf("임의의 정수 입력 : ");
+	// 사용자로부터 데이터를 입력
+	printf("임의의 정수 입력: ");
 	scanf_s("%d", &a);		// scanf("%d", &a);
 
 	// 사용자에게 입력 받은 데이터 출력
-	printf("a : %d \n", a);
+	printf("a: %d \n", a);
 
 	return 0;
 }
