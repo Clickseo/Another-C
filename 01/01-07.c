fe1f91451a7f28d9ea06abb39b8850a51073934f
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2021-03-06 19:16:37 +0900

    Update 01-07.c

--- 01/01-07.c
+++ 01/01-07.c
@@ -1,10 +1,10 @@
-// 64bits(8bytes) 정수형 : long long int
+// 64bits(8bytes) 정수형: long long int
 #include <stdio.h>
 int main(void)
 {
 	long long int		a = 9223372036854775807;
 
-	printf("a : %lld \n", a);		// %LLd
+	printf("a: %lld \n", a);		// %LLd
 
 	return 0;
 }
