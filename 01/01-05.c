cba5d75c24e189d9acf103cb673be22e41c56250
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2021-03-06 19:13:49 +0900

    Update 01-05.c

--- 01/01-05.c
+++ 01/01-05.c
@@ -1,7 +1,10 @@
+// main 함수 -- 명령행 인자
 #include <stdio.h>
-int main(int argc, char *argv[])
+
+// int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
 	printf("Hello, world!!! \n");
-	
+
 	return 0;
 }
