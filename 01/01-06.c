2707b5b943a3c37444955558e9b2a244d4b8b62d
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2021-03-06 19:16:11 +0900

    Update 01-06.c

--- 01/01-06.c
+++ 01/01-06.c
@@ -1,11 +1,11 @@
-// 64bits(8bytes) 정수형 : __int64
+// 64bits(8bytes) 정수형: __int64
 #include <stdio.h>
 int main(void)
 {
 	// long long int	a = 9223372036854775807;
 	__int64			a = 9223372036854775807;
 
-	printf("a : %I64d \n", a);	// %I64
+	printf("a: %I64d \n", a);	// %I64
 
 	return 0;
 }
