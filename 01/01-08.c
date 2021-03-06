8f226a22447e2c2991b6460f0f766e3ed1daca85
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2021-03-06 19:17:28 +0900

    Update 01-08.c

--- 01/01-08.c
+++ 01/01-08.c
@@ -1,11 +1,11 @@
-// 다양한 정수형의 메모리 크기
+// 다양한 정수형의 메모리 할당 크기: sizeof 연산자
 #include <stdio.h>
 int main(void)
 {
-	printf("    short int : %d바이트\n", sizeof(short int));		// short
-	printf("          int : %d바이트\n", sizeof(int));			// int
-	printf("     long int : %d바이트\n", sizeof(long int));		// long
-	printf("long long int : %d바이트\n", sizeof(long long int));	// long long
+	printf("    short int: %d바이트\n", sizeof(short int));		// short
+	printf("          int: %d바이트\n", sizeof(int));			// int
+	printf("     long int: %d바이트\n", sizeof(long int));		// long
+	printf("long long int: %d바이트\n", sizeof(long long int));	// long long
 	
 	return 0;
 }
