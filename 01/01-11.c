877c3de58df74b5494f7a797f17d04ce04a09aca
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2021-03-06 19:22:13 +0900

    Update 01-11.c

--- 01/01-11.c
+++ 01/01-11.c
@@ -5,8 +5,8 @@ int main(void)
 
 	// %d : 부호 있는 10진수 출력(signed)
 	// %u : 부호 없는 10진수 출력(unsigned)
-	printf("i : %d \n", i);			// -1
-	printf("i : %u \n", i);			// 4294967295
+	printf("i: %d \n", i);			// -1
+	printf("i: %u \n", i);			// 4294967295
 
 	return 0;
 }
