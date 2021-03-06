32cb1ef8b6738754ba8e6d9364fdd772bf92ecfb
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2021-03-06 19:22:34 +0900

    Update 01-12.c

--- 01/01-12.c
+++ 01/01-12.c
@@ -6,11 +6,11 @@ int main(void)
 
 	// %f : 실수 출력(소수점 이하 6자리까지 출력)
 	// %e : 실수 출력(과학 기술 계산용 표기법)
-	printf("a : %f \n", a);		// 0.000000
-	printf("a : %e \n", a);		// 1.000000e-009
+	printf("a: %f \n", a);		// 0.000000
+	printf("a: %e \n", a);		// 1.000000e-009
 
-	printf("a : %f \n", b);		// 3.141593
-	printf("a : %e \n", b);		// 3.141593e+000
+	printf("a: %f \n", b);		// 3.141593
+	printf("a: %e \n", b);		// 3.141593e+000
 
 	return 0;
 }
