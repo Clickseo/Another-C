3dd82c75e2362bec05b58f639a38030ccc65810d
Seo, Doo-Ok <34378220+Clickseo@users.noreply.github.com>
2021-03-06 19:21:23 +0900

    Update 01-10.c

--- 01/01-10.c
+++ 01/01-10.c
@@ -1,12 +1,17 @@
-// 영문 소문자(a-z) 출력 : ASCII 코드 값
+// 영문 소문자(a-z) 출력: ASCII 코드
 #include <stdio.h>
 int main(void)
 {
-	char	ch = 97;				// char	ch = 'a';
-	for(int i = 1; ch <= 122; ch++, i++)		// ( ch <= 122 ) == ( ch <= 'z' )
-	{
-		if(i%5)	printf("%3c : %3d \t", ch, ch);
-		else	printf("%3c : %3d \n", ch, ch);
+	// 'a' == 97
+	// 'b' == 98
+	// ...
+	// 'z' = 122
+
+	// char		ch = 'a';
+	char		ch = 97;
+	for (int i = 1; ch <= 122; ch++, i++)	{
+		if (i%5)	printf("%3c: %3d \t", ch, ch);
+		else		printf("%3c: %3d \n", ch, ch);
 	}
 	printf("\n");
 
