f1db71f5f63eaa08508ee17ae33c9384c9c65b2b
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2023-11-18 04:40:34 +0900

    Update 08-05.c

--- 08/08-05.c
+++ 08/08-05.c
@@ -16,7 +16,7 @@ typedef struct bitFieldDATE {
 	unsigned int	year : 16;	// 0 ~ 9999	// 16 bits
 	unsigned int	month : 4;	// 1 ~ 12	//  4 bits
 	unsigned int	day : 5;	// 1 ~ 31	//  5 bits
-	unsigned char : 7;		// Reserved	// Padding Bits
+	unsigned int : 7;		// Reserved	// Padding Bits
 } bitFieldDATE;
 
 int main(void)
