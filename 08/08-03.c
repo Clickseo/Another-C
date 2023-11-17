e108079f59c1ad7dfdc2c73f54bf1dd61ebe9e4f
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2023-11-18 04:26:46 +0900

    Update 08-03.c

--- 08/08-03.c
+++ 08/08-03.c
@@ -1,28 +1,27 @@
-// 구조체 선언 및 정의: 구조체 멤버 접근
+// 구조체 형식 정의 및 선언: 구조체 멤버 접근
 #include <stdio.h>
-#include <string.h>	// strcpy / strcpy_s
+#include <string.h>	// strcpy, strcpy_s
 int main(void)
 {
-	// 구조체형 변수
+	// 구조체 변수
 	struct {
 		char	name[12];
 		int	kor, eng, math, tot;
 		float	ave;
-	}s;
+	}temp;
 
 	// 구두점 연산자(.): 구조체 멤버 접근
-	// 구조체 멤버에 데이터 저장
-	// strcpy(s.name, "홍길동");
-	strcpy_s(s.name, sizeof(s.name), "홍길동");
-	s.kor = 70;
-	s.eng = 80;
-	s.math = 90;
+	// strcpy(temp.name, "홍길동");
+	strcpy_s(temp.name, sizeof(temp.name), "홍길동");
+	temp.kor = 70;
+	temp.eng = 80;
+	temp.math = 90;
 
-	s.tot = s.kor + s.eng + s.math;
-	s.ave = (float)(s.kor + s.eng + s.math) / 3;
+	temp.tot = temp.kor + temp.eng + temp.math;
+	temp.ave = (float)(temp.kor + temp.eng + temp.math) / 3;
 
 	printf("%10s, %3d, %3d, %3d, %5d, %8.2lf \n",
-		s.name, s.kor, s.eng, s.math, s.tot, s.ave);
+		temp.name, temp.kor, temp.eng, temp.math, temp.tot, s.ave);
 
 	return 0;
 }
