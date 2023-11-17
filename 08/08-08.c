f68dca1a452ab42709e08a268594de8cea9c49b5
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2023-11-18 04:55:33 +0900

    Update 08-08.c

--- 08/08-08.c
+++ 08/08-08.c
@@ -1,44 +1,45 @@
-// 구조체의 이해: 중첩 구조체
+// 중첩 구조체(Nested Struct)
 #include <stdio.h>
 
-// 형식(재) 정의된 구조체: DATE
+// 구조체 형식 정의: 날짜(DATE)
 typedef struct _date {
-	int		year;
-	int		month;
-	int		day;
-}DATE;
+	int	year;
+	int	month;
+	int	day;
+} DATE;
 
-// 형식(재) 정의된 구조체: INFO
+// 구조체 형식 정의: 정보(INFO)
 typedef struct _info {
-	char	name[12];
-	DATE	birth_date;	// 생년월일
-	DATE	join_date;	// 입사일
-	DATE	resign_date;	// 퇴사일
-}INFO;
+	char	name[12];	// 이름
+	DATE	birthDate;	// 생년월일
+	DATE	joinDate;	// 입사일
+	DATE	resignDate;	// 퇴사일
+} INFO;
 
 int main(void)
 {
+	// 구조체 변수 선언
 	INFO	temp;
 
 	printf("이    름: ");
 	gets_s(temp.name, sizeof(temp.name));		// gets(temp.name);
 
 	printf("생년월일: ");
-	// scanf("%d %d %d", &temp.birth_date.year, &temp.birth_date.month, &temp.birth_date.day);
-	scanf_s("%d %d %d", &temp.birth_date.year, &temp.birth_date.month, &temp.birth_date.day);
+	scanf_s("%d %d %d", &temp.birthDate.year, &temp.birthDate.month, &temp.birthDate.day);
+	// scanf("%d %d %d", &temp.birthDate.year, &temp.birthDate.month, &temp.birthDate.day);
 
-	printf("입사날짜: ");
-	// scanf("%d %d %d", &temp.join_date.year,	&temp.join_date.month, &temp.join_date.day);
-	scanf_s("%d %d %d", &temp.join_date.year, &temp.join_date.month, &temp.join_date.day);
+	printf("입사일: ");
+	scanf_s("%d %d %d", &temp.joinDate.year, &temp.joinDate.month, &temp.joinDate.day);
+	// scanf("%d %d %d", &temp.joinDate.year, &temp.joinDate.month, &temp.joinDate.day);
 
-	printf("퇴사날짜: ");
-	// scanf("%d %d %d", &temp.resign_date.year, &temp.resign_date.month, &temp.resign_date.day);
-	scanf_s("%d %d %d", &temp.resign_date.year, &temp.resign_date.month, &temp.resign_date.day);
+	printf("퇴사일: ");
+	scanf_s("%d %d %d", &temp.resignDate.year, &temp.resignDate.month, &temp.resignDate.day);
+	// scanf("%d %d %d", &temp.resignDate.year, &temp.resignDate.month, &temp.resignDate.day);
 
 	printf("\n %10s님의 정보 출력\n", temp.name);
-	printf("생년월일: %d %d %d\n", temp.birth_date.year, temp.birth_date.month, temp.birth_date.day);
-	printf("입사날짜: %d %d %d\n", temp.join_date.year,	temp.join_date.month, temp.join_date.day);
-	printf("퇴사날짜: %d %d %d\n", temp.resign_date.year, temp.resign_date.month, temp.resign_date.day);
+	printf("생년월일: %d %d %d\n", temp.birthDate.year, temp.birthDate.month, temp.birthDate.day);
+	printf("입사날짜: %d %d %d\n", temp.joinDate.year, temp.joinDate.month, temp.joinDate.day);
+	printf("퇴사날짜: %d %d %d\n", temp.resignDate.year, temp.resignDate.month, temp.resignDate.day);
 
 	return 0;
 }
