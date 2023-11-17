d037199ad7403079e2e8c3d1e876d355a3c68095
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2023-11-18 04:58:56 +0900

    Update 08-17.c

--- 08/08-17.c
+++ 08/08-17.c
@@ -1,17 +1,17 @@
-// 복합 데이터 유형: 열거형
+// 복합 데이터 유형: 열거형(Enumerated Type)
 #include <stdio.h>
-// #include <stdbool.h>		// bool, true, false
+#include <stdbool.h>	// bool, true, false
 
 // 매크로 상수: 참(TRUE)과 거짓(FALSE)
 // #define TRUE 1
 // #define FALSE 0
 
-enum BOOL { FALSE, TRUE };
-
 int main(void)
 {
+	// 열거형 정의
+	enum BOOL { FALSE, TRUE };
+	
 	int	state = TRUE;		// int  state = 1;
-
 	if (state == TRUE)		// if(state == 1)
 		printf("참!!! \n");
 	else if (state == FALSE)	// if(state == 0)
