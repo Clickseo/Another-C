3eaa14de2f7de4693d8671840982218007e8ca2a
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-16 04:06:46 +0900

    Update 01-02.c

--- 01/01-02.c
+++ 01/01-02.c
@@ -6,7 +6,8 @@ int main(void)
 
 	// 사용자로부터 데이터를 입력받아 메모리에 저장
 	printf("임의의 정수 입력: ");
-	scanf_s("%d", &a);		// scanf("%d", &a);
+	scanf_s("%d", &a);
+	// scanf("%d", &a);
 
 	// 메모리에 저장된 데이터를 화면에 출력
 	printf("a: %d \n", a);
