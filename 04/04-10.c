69dbeda04942841c971dc51798c2caaaa69e1eda
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-16 04:17:28 +0900

    Update 04-10.c

--- 04/04-10.c
+++ 04/04-10.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 int main(void)
 {
-	int	kor, eng, math, tot;
+	int	kor, eng, math, sum;
 	double	ave;
 
 	printf("세 과목의 점수 입력 : ");
-	scanf_s("%d %d %d", &kor, &eng, &math);		// scanf("%d %d %d", &kor, &eng, &math);
+	scanf_s("%d %d %d", &kor, &eng, &math);
+	// scanf("%d %d %d", &kor, &eng, &math);
 
-	tot = kor + eng + math;
+	sum = kor + eng + math;
 	ave = (kor + eng + math) / 3.0;
+	// ave = (double)(kor + eng + math) / 3;
 
-	printf("총점 : %d, 평균 : %.2lf \n", tot, ave);
+	printf("총점: %d, 평균: %.2lf \n", sum, ave);
 
 	return 0;
 }
