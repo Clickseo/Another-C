3656f6328f1000ec1350d08db2cd890499a57cea
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-16 04:07:09 +0900

    Update 01-03.c

--- 01/01-03.c
+++ 01/01-03.c
@@ -9,13 +9,16 @@ int main(void)
 
 	// 사용자로 데이터(년월일)을 입력 받아 메모리에 저장
 	printf("년: ");
-	scanf_s("%d", &year);		// scanf("%d", &year);
+	scanf_s("%d", &year);
+	// scanf("%d", &year);
 
 	printf("월: ");
-	scanf_s("%d", &month);		// scanf("%d", &month);
+	scanf_s("%d", &month);
+	// scanf("%d", &month);
 
 	printf("일: ");
-	scanf_s("%d", &day);		// scanf("%d", &day);
+	scanf_s("%d", &day);
+	// scanf("%d", &day);
 
 	// 메모리의 데이터(년월일)를 화면에 출력
 	printf("오늘의 날짜: %d-%d-%d \n", year, month, day);
