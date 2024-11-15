5a2fe62df8719ea7e6b8f688d07415aa19e82943
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-16 04:20:12 +0900

    Update 04-18.c

--- 04/04-18.c
+++ 04/04-18.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-// 함수 원형 선언 : DIV 함수 -- 몫과 나머지 계산
+// 함수 원형 선언: DIV 함수 -- 몫과 나머지 계산
 void	DIV(int, int, int *, int *);
 int main(void)
 {
 	int	a = 10, b = 3;
 	int	quot, rem;
 
-	// 함수 호출 : DIV 함수 -- 몫과 나머지 계산
+	// 함수 호출: DIV 함수 -- 몫과 나머지 계산
 	DIV(a, b, &quot, &rem);
 
 	printf("몫 : %d, 나머지 : %d \n", quot, rem);	// 3, 1
@@ -15,7 +15,7 @@ int main(void)
 	return 0;
 }
 
-// 함수 정의 : DIV 함수 -- 몫과 나머지 계산
+// 함수 정의: DIV 함수 -- 몫과 나머지 계산
 void	DIV(int a, int b, int *pQuot, int *pRem) {
 	*pQuot = a / b;		// quot = a / b;
 	*pRem = a % b;		// rem = a % b;
