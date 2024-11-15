9b1d554c57684dceebc0dafc9b4b65952341825b
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-16 04:10:39 +0900

    Update 02-14.c

--- 02/02-14.c
+++ 02/02-14.c
@@ -4,10 +4,10 @@ int main(void)
 	int	a = 1024;
 
 	printf("a      = %d\n", a);			// 1024
-	printf("a >> 1 = %d\n", a >> 1);		//  512
-	printf("a >> 2 = %d\n", a >> 2);		//  256
-	printf("a >> 3 = %d\n", a >> 3);		//  128
-	printf("a >> 4 = %d\n", a >> 4);		//  64
+	printf("a >> 1 = %d\n", a >> 1);		// 512
+	printf("a >> 2 = %d\n", a >> 2);		// 256
+	printf("a >> 3 = %d\n", a >> 3);		// 128
+	printf("a >> 4 = %d\n", a >> 4);		// 64
 
 	return 0;
 }
