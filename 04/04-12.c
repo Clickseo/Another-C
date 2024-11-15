c2fd033f055652f44fc223ded0f402331a84c5fc
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-16 04:18:34 +0900

    Update 04-12.c

--- 04/04-12.c
+++ 04/04-12.c
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 
-// 함수 원형 선언 : Factorial 함수
+// 함수 원형 선언: Factorial 함수
 int  Factorial(int  num);
 
 int main(void)
@@ -14,7 +14,8 @@ int main(void)
 	int	num;
 
 	printf("임의의 정수 입력 : ");
-	scanf_s("%d", &num);		// scanf("%d", &num);
+	scanf_s("%d", &num);
+	// scanf("%d", &num);
 
 	// Factorial 함수 호출
 	printf("Factorial %d : %d \n", num, Factorial(num) );
@@ -22,11 +23,11 @@ int main(void)
 	return 0;
 }
 
-// 함수 정의 : Factorial 함수 -- 재귀적 용법
+// 함수 정의: Factorial 함수 -- 재귀적 용법
 int  Factorial(int  num) {	
 	if(num <= 1)		// 재귀 함수의 탈출 조건
 		return 1;
 
-	// 재귀 호출 : Factorial 함수 내부에서 Factorial 함수 호출
+	// 재귀 호출: Factorial 함수 내부에서 Factorial 함수 호출
 	return  num * Factorial(num - 1);
 }
