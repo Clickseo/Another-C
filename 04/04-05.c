b07edc0cad0629e87889cc23f6a27d8bb42434c6
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-16 04:15:05 +0900

    Update 04-05.c

--- 04/04-05.c
+++ 04/04-05.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-// 함수 원형 선언 : OUTPUT 함수
+// 함수 원형 선언: OUTPUT 함수
 void OUTPUT(void);
 
 int main(void)
 {
-	// 함수 호출 : OUTPUT 함수
+	// 함수 호출: OUTPUT 함수
 	OUTPUT();
 
 	return 0;
 }
 
-// 함수 정의 : OUTPUT 함수
+// 함수 정의: OUTPUT 함수
 void OUTPUT(void) {
 	printf("Hello World!!! \n");
 }
