f021c0ce6f40de73d9bc3b0027c15bcba2a7fd72
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-16 04:18:13 +0900

    Update 04-11.c

--- 04/04-11.c
+++ 04/04-11.c
@@ -9,23 +9,24 @@ int main(void)
 	int	kor, eng, math, tot;
 	double	ave;
 
-	printf("3과목의 점수 입력 : ");
-	scanf_s("%d %d %d", &kor, &eng, &math);		// scanf("%d %d %d", &kor, &eng, &math);
+	printf("3과목의 점수 입력: ");
+	scanf_s("%d %d %d", &kor, &eng, &math);
+	// scanf("%d %d %d", &kor, &eng, &math);
 
-	// 직접 계산 : 총점과 평균 계산 -- 단일 함수(main 함수)
-	// tot = kor + eng + math;
+	// 직접 계산: 총점과 평균 계산 -- 단일 함수(main 함수)
+	// sum = kor + eng + math;
 	// ave = (kor + eng + math) / 3.0;
 
 	// 함수 호출 : 총점(SUM 함수)과 평균(AVE 함수) 계산
-	tot = SUM(kor, eng, math);
+	sum = SUM(kor, eng, math);
 	ave = AVE(kor, eng, math);
 
-	printf("총점 : %d, 평균 : %.2lf \n", tot, ave);
+	printf("총점: %d, 평균: %.2lf \n", tot, ave);
 
 	return 0;
 }
 
-// 함수 정의 : SUM 함수 -- 3과목에 대한 총점 계산
+// 함수 정의: SUM 함수 -- 3과목에 대한 총점 계산
 int	SUM(int a, int b, int c) {
 	return a + b + c;
 }
