663be53456bdd32e2607e0af3497031e4c934037
무명(無名) <34378220+Clickseo@users.noreply.github.com>
2024-11-16 04:16:18 +0900

    Update 04-08.c

--- 04/04-08.c
+++ 04/04-08.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// 함수 원형 선언 : MAX 함수
+// 함수 원형 선언: MAX 함수
 int	MAX(int, int, int, int);
 
 int main(void)
@@ -8,19 +8,19 @@ int main(void)
 	int	a, b, c, d, max;
 
 	printf("4개의 정수 입력 : ");
-	scanf_s("%d %d %d %d", &a, &b, &c, &d);		// scanf("%d %d %d %d", &a, &b, &c, &d);
+	scanf_s("%d %d %d %d", &a, &b, &c, &d);	// scanf("%d %d %d %d", &a, &b, &c, &d);
 
-	// 함수 호출 : MAX 함수 -- 4개의 정수 중 최대값 계산
+	// 함수 호출: MAX 함수 -- 4개의 정수 중 최댓값 계산
 	max = MAX(a, b, c, d);
 
-	printf("최대값 : %d \n", max);
+	printf("최댓값: %d \n", max);
 
 	return 0;
 }
 
-// 함수 정의 : MAX 함수 -- 전달(입력)받은 4개의 정수 중에서 최대값 반환
+// 함수 정의: MAX 함수 -- 전달(입력)받은 4개의 정수 중에서 최댓값 반환
 int	MAX(int a, int b, int c, int d) {
-	int		max;
+	int	max;
 
 	// 4개의 정수 중에서 최대값을 구하는 부분
 	if(a > b)	max = a;
